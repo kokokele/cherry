9f4f5f28faddfa354a343a5268f34e91b15bccd6
kokokele <zp1030@gmail.com>
2017-06-08 19:13:31 +0800

    add test

---
+++ __test__/tmp/__testCategory_testPage.js
@@ -0,0 +1,3 @@
+module.exports = [
+require('/Users/zhangpeng53/Documents/my/markdoc/__test__/md/a.md'),
+]
--- __test__/walkmd.test.js
+++ __test__/walkmd.test.js
@@ -3,18 +3,25 @@ const path = require("path");
 
 const config = {
     root: path.resolve(__dirname,  './md'),
-    ext: '.md'
+    ext: '.md',
+    theme: __dirname
+
 }
 
 describe('test walkmd', () => {
-    
-    it('should data is right', (done) => {
-        walkmd(config, (data) => {
-            console.log(data)
-            expect(data.testCategory[0].page).toEqual('testPage');
-            done();
-        })
+
+    // it('should data is right', (done) => {
+    //     walkmd(config, (data) => {
+    //         console.log(data)
+    //         expect(data.testCategory[0].page).toEqual('testPage');
+    //         done();
+    //     })
+    // });
+
+    it('测试__md__.json 数据是否正确', async () => {
+        await walkmd(config);
+        const __md__ = require('./tmp/__md__.json');
+        expect(__md__.md.testCategory[0].page).toEqual('testPage');
     });
-    
-   
+
 });
